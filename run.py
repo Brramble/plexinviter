d604e889416215fbdcee35a05d608b9487dbce44
Bramble <Brramble@users.noreply.github.com>
2024-02-04 20:54:47 +0000

    Fix Webpage title

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from modules.plex_invitation import send_plex_invitation, send_email
+from config import WEBPAGE_TITLE
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', WEBPAGE_TITLE=WEBPAGE_TITLE)
 
 @app.route('/send_invitation', methods=['POST'])
 def send_invitation():
@@ -21,6 +22,5 @@ def send_invitation():
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
-    #app.run(debug=True, port=3500)
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
