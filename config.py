70c89cebc165ce5a8224bae2b63e372a2e39e867
Bramble <Brramble@users.noreply.github.com>
2024-02-04 20:53:55 +0000

    Migration to config yaml

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-PLEX_TOKEN = ""
-PLEX_SERVER_URL = ""
+import yaml
+import os
 
-SMTP_USERNAME = ""
-SMTP_PASSWORD = ""
-SMTP_SERVER = ""
-SMTP_PORT = 587
-EMAIL_SENDER = "inviter@example.com"
-EMAIL_DISPLAYNAME = "Server Name"
+with open("config.yaml", "r") as yaml_file:
+    config = yaml.safe_load(yaml_file)
 
-EMAIL_SUBJECT = "Welcome to the server"
-EMAIL_MESSAGE = """Hi,
-
-Here's some exmaple text... etc... etc...
-
-Thanks.
-"""
+PLEX_TOKEN = config.get("CONFIG", {}).get("PLEX_TOKEN", "")
+PLEX_SERVER_URL = config.get("CONFIG", {}).get("PLEX_SERVER_URL", "")
+SMTP_USERNAME = config.get("CONFIG", {}).get("SMTP_USERNAME", "")
+SMTP_PASSWORD = config.get("CONFIG", {}).get("SMTP_PASSWORD", "")
+SMTP_SERVER = config.get("CONFIG", {}).get("SMTP_SERVER", "")
+SMTP_PORT = config.get("CONFIG", {}).get("SMTP_PORT", 587)
+EMAIL_SENDER = config.get("CONFIG", {}).get("EMAIL_SENDER", "")
+EMAIL_DISPLAYNAME = config.get("CONFIG", {}).get("EMAIL_DISPLAYNAME", "")
+EMAIL_SUBJECT = config.get("CONFIG", {}).get("EMAIL_SUBJECT", "Your invite is here")
+EMAIL_MESSAGE = config.get("CONFIG", {}).get("EMAIL_MESSAGE", "")
+WEBPAGE_TITLE = config.get("CONFIG", {}).get("WEBPAGE_TITLE", "Invitation Dashboard")
