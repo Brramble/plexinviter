e1f2a9d6ca73d41f27e898ca0500cfdd6ce0f89d
Bramble <Brramble@users.noreply.github.com>
2024-02-04 17:55:42 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+PLEX_TOKEN = ""
+PLEX_SERVER_URL = ""
+
+SMTP_USERNAME = ""
+SMTP_PASSWORD = ""
+SMTP_SERVER = ""
+SMTP_PORT = 587
+EMAIL_SENDER = "inviter@example.com"
+EMAIL_DISPLAYNAME = "Server Name"
+
+EMAIL_SUBJECT = "Welcome to the server"
+EMAIL_MESSAGE = """Hi,
+
+Here's some exmaple text... etc... etc...
+
+Thanks.
+"""
